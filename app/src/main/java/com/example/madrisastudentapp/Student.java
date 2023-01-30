6a517970f6b22483c1d5235f0116577acaf2ce6b
Muhammad Amaan <bitf19m016@pucit.edu.pk>
2023-01-30 23:41:49 +0500

    More code implemented in the class

--- app/src/main/java/com/example/madrisastudentapp/Student.java
+++ app/src/main/java/com/example/madrisastudentapp/Student.java
@@ -10,5 +10,79 @@ public class Student {
     String sabaqiComments;
     String manzilComments;
 
-    
+    public Student(String Name, String Date, int[] Sabaq, int Sabaqi, int Manzil, String sabaqComments, String sabaqiComments, String manzilComments){
+        this.Date = Date;
+        this.Name = Name;
+        this.Manzil = Manzil;
+        this.manzilComments = manzilComments;
+        this.Sabaq = Sabaq;
+        this.sabaqComments = sabaqComments;
+        this.Sabaqi = Sabaqi;
+        this.sabaqiComments = sabaqiComments;
+    }
+
+    public String getDate() {
+        return Date;
+    }
+
+    public void setDate(String date) {
+        Date = date;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String Name) {
+        this.Name = Name;
+    }
+
+    public int[] getSabaq() {
+        return Sabaq;
+    }
+
+    public void setSabaq(int[] Sabaq) {
+        this.Sabaq = Sabaq;
+    }
+
+    public int getSabaqi() {
+        return Sabaqi;
+    }
+
+    public void setSabaqi(int Sabaqi) {
+        this.Sabaqi = Sabaqi;
+    }
+
+    public int getManzil() {
+        return Manzil;
+    }
+
+    public void setManzil(int Manzil) {
+        this.Manzil = Manzil;
+    }
+
+    public String getmanzilComments() {
+        return manzilComments;
+    }
+
+    public void setmanzilComments(String manzilComments) {
+        this.manzilComments = manzilComments;
+    }
+
+    public String getsabaqComments() {
+        return sabaqComments;
+    }
+
+    public void setsabaqComments(String sabaqComments) {
+        this.sabaqComments = sabaqComments;
+    }
+
+    public String getsabaqiComments() {
+        return sabaqiComments;
+    }
+
+    public void setsabaqiComments(String sabaqiComments) {
+        this.sabaqiComments = sabaqiComments;
+    }
+
 }
