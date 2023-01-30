dbab156c42965bef81d42dd54686c4957f5140f0
Muhammad Amaan <bitf19m016@pucit.edu.pk>
2023-01-30 23:30:21 +0500

    Student Class added

---
+++ app/src/main/java/com/example/madrisastudentapp/Student.java
@@ -0,0 +1,14 @@
+package com.example.madrisastudentapp;
+
+public class Student {
+    String Name;
+    String Date;
+    int[] Sabaq; //array of 3 elements, 1st element --> Surah Number other 2 will be ayah number, starting and ending
+    int Sabaqi; //Previous Parah
+    int Manzil; //From 1st parah till Sabqi
+    String sabaqComments;
+    String sabaqiComments;
+    String manzilComments;
+
+    
+}
