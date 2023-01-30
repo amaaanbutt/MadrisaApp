703007de29a1452a0a31e1d1614ad3a9a2a31520
Muhammad Amaan <bitf19m016@pucit.edu.pk>
2023-01-30 23:44:22 +0500

    DBHandler added

---
+++ app/src/main/java/com/example/madrisastudentapp/DBHandler.java
@@ -0,0 +1,9 @@
+package com.example.madrisastudentapp;
+
+public class DBHandler {
+    static final String DATABASE_NAME = "Student_Record";
+
+    static final String TABLE_NAME = "STUDENT";
+    static final String STUDENT_NAME = "NAME";
+    static final String COLUMN_DATE = "DATE";
+}
